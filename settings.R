316e710a5258115038909b7db0922721d852365c
Bill DeVoe <39626006+bdevoe@users.noreply.github.com>
2019-11-18 07:40:22 -0500

    Create settings.R

---
+++ settings.R
@@ -0,0 +1,4 @@
+### Settings for portal - used to prevent hardcoding in app
+
+# URL to the form to fill in to obtain a log in
+form_url <- "https://forms.office.com/Pages/ResponsePage.aspx?id=q6g_QX0gYkubzeoajy-GTv8R9Ka_xMNCo1OoryQyHMdUME5PWFRDVzNKWks5MUxJMUwwSlFEUEtMOC4u"
