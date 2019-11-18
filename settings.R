0e9ac21b277252f4dbd003e9727a80d1c0a777c0
Bill DeVoe <39626006+bdevoe@users.noreply.github.com>
2019-11-18 08:35:01 -0500

    Update settings.R

--- settings.R
+++ settings.R
@@ -1,4 +1,4 @@
 ### Settings for portal - used to prevent hardcoding in app
 
 # URL to the form to fill in to obtain a log in
-form_url <- "https://forms.office.com/Pages/ResponsePage.aspx?id=q6g_QX0gYkubzeoajy-GTv8R9Ka_xMNCo1OoryQyHMdUME5PWFRDVzNKWks5MUxJMUwwSlFEUEtMOC4u"
+gbl_form_url <- "https://forms.office.com/Pages/ResponsePage.aspx?id=q6g_QX0gYkubzeoajy-GTv8R9Ka_xMNCo1OoryQyHMdUME5PWFRDVzNKWks5MUxJMUwwSlFEUEtMOC4u"
