fda8957fafe6bf70e928fea6c05e22e525e115c7
Bill DeVoe <39626006+bdevoe@users.noreply.github.com>
2025-08-05 15:29:56 -0400

    Update settings.R

--- settings.R
+++ settings.R
@@ -2,3 +2,6 @@
 
 # URL to the form to fill in to obtain a log in
 gbl_form_url <- "https://forms.office.com/Pages/ResponsePage.aspx?id=q6g_QX0gYkubzeoajy-GTv8R9Ka_xMNCo1OoryQyHMdUME5PWFRDVzNKWks5MUxJMUwwSlFEUEtMOC4u"
+
+# Url to load the Markdown About file
+about_url <- "https://raw.githubusercontent.com/mainedmr/Trawl_Survey_Portal/master/about.md"
